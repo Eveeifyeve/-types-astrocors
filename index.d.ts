c50de3d5deb4826d7f101c88ff1d97ceedbc096f
eveeifyeve <eveeg1971@gmail.com>
2024-05-09 08:18:49 +1000

    feat: types and packages needed

--- index.d.ts
+++ index.d.ts
@@ -1,24 +1,22 @@
 declare module "corsMiddlewareModule" {
-  export interface Request {
-    method: string;
-    // Add other properties as needed
-  }
+	export interface Request {
+		method: string;
+		// Add other properties as needed
+	}
 
-  export interface NextFunction {
-    (): Promise<Response>;
-  }
+	export type NextFunction = () => Promise<Response>;
 
-  export interface Response {
-    headers: Headers;
-    body: any; // Specify the actual type based on your application's needs
-  }
+	export interface Response<T = unknown> {
+		headers: Headers;
+		body: T;
+	}
 
-  export interface Headers {
-    append(key: string, value: string): void;
-  }
+	export interface Headers {
+		append(key: string, value: string): void;
+	}
 
-  export function corsMiddleware(
-    request: Request,
-    next: NextFunction
-  ): Promise<Response>;
+	export function corsMiddleware(
+		request: Request,
+		next: NextFunction,
+	): Promise<Response>;
 }
